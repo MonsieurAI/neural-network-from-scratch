4c6ecd95974079ec003d1a43686078f6e34929bd
MonsieurAI <davidtomijuK@gmail.com>
2026-03-14 11:06:48 +0200

    Implement base tensor class with 0D/1D/2D data initialization
    - Define internal structure for data, shape, and stride.
    - Add constructors for scalar, 1D, and 2D tensor generation.
    - Implement row major flattening for 2D tensors.
    - Add dimension validation

--- include/tensor.h
+++ include/tensor.h
@@ -1,1 +1,13 @@
 #pragma once
+#include <vector>
+
+class Tensor {
+    private:
+        std::vector<float> _data;
+        std::vector<size_t> _shape;
+        std::vector<size_t> _stride;
+    public:
+        Tensor(float data);
+        Tensor(std::vector<float> data);
+        Tensor(std::vector<std::vector<float>> data);
+};
--- src/tensor.cpp
+++ src/tensor.cpp
@@ -1,1 +1,25 @@
 #include "../include/tensor.h"
+#include <stdexcept>
+
+Tensor::Tensor(float data) : _data{data}, _shape{}, _stride{} {};
+
+Tensor::Tensor(std::vector<float> data) : _data(data), _shape{data.size()}, _stride{1} {};
+
+Tensor::Tensor(std::vector<std::vector<float>> data)
+    : _shape{data.size(), data[0].size()}, _stride{data[0].size(), 1} {
+
+        // Check dimension consistency
+        size_t expected_size = data[0].size();
+        for (size_t i = 0; i < data.size(); i++) {
+            if (data[i].size() != expected_size) {
+                throw std::invalid_argument("Dimensions are incositent");
+            }
+        }
+
+        // Store data in row major format
+        for (size_t i = 0; i < data.size(); i++) {
+            for (size_t j = 0; j < data[0].size(); j++) {
+                _data.push_back(data[i][j]);
+            }
+        }
+    }
